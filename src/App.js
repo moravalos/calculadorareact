bebe81dae221c0b88fe62af83dbeb3c90778451d
Marcos Mora <eduardoavalos399@gmail.com>
2022-06-12 17:37:34 -0500

    feat:add digits function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
 const createDigits = () => {
   const digits = [];
 
-  for (let i=1; i < 10; i++){
+  for (let i=0; i < 10; i++){
     digits.push(
       <button key={i}>{i}</button>
     )
@@ -25,7 +25,6 @@ const createDigits = () => {
           </div>
           <div className="digitos">
             {createDigits()}
-            <button>0</button>
             <button>.</button>
             <button>=</button>
           </div>
